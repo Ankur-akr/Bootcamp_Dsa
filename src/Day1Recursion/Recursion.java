1e54050e3f8a30ebbf5f6dc215f6f15b8dd3cc5b
Ankur <you@example.com>
2026-06-02 13:57:09 +0530

    Day1

--- src/Day1Recursion/Recursion.java
+++ src/Day1Recursion/Recursion.java
@@ -2,7 +2,15 @@ package Day1Recursion;
 
 public class Recursion {
     //Function calling itself
+    public static  void show(int counter){
+        //Show() - exactly 5 times
+        if (counter <=5){          //base case
+            System.out.println("show() - "+counter);
+            show(counter += 1);
+            System.out.println("BackTrack - "+counter);
+        }
+    }
     public static void main(String[] args) {
-
+        show(1);
     }
 }
