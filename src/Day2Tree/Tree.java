393f8a04557ec71a05cde8fcab577ca1713ba18c
Ankur Rai <ankurrai5711@gmail.com>
2026-06-04 23:36:47 +0530

    Tree Traversal(DFS)
    :wq
    
    :wq
    
    :wq

--- src/Day2Tree/Tree.java
+++ src/Day2Tree/Tree.java
@@ -16,7 +16,27 @@ public class Tree {
     Tree(){
         root = null;
     }
-
+    public void inorderTraversal (Node node){
+        if(node != null){
+            inorderTraversal(node.left);
+            System.out.print(node.data + " -->");
+            inorderTraversal(node.right);
+        }
+    }
+    public void preOrder(Node node){
+        if(node != null){
+            System.out.print(node.data + " -->");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+    public void postOrder(Node node){
+        if(node != null){
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.data + " -->");
+        }
+    }
     public static void main(String[] args) {
 
         Tree tree1 = new Tree();
@@ -24,9 +44,19 @@ public class Tree {
         tree1.root=new Node(10);  // because of using new keyword so the node have physical address also
         System.out.println(tree1.root);
         tree1.root.left=new Node(20);
+        System.out.println(tree1.root.left.data);
         tree1.root.right=new Node(30);
         tree1.root.left.left=new Node(40);
         tree1.root.left.right=new Node(50);
+        tree1.root.right.left = new Node(60);
+        tree1.root.right.right = new Node(70);
+        tree1.inorderTraversal(tree1.root);
+        System.out.println(" ");
+        System.out.println("preorder");
+        tree1.preOrder(tree1.root);
+        System.out.println(" postorder");
+        tree1.postOrder(tree1.root);
     }
 }
 //null is not applied to primitive datatype
+//lc 94,144,145,104
