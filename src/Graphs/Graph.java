17db384d71ae3888a72fe3a57cfcf5f2a71210eb
Ankur Rai <ankurrai5711@gmail.com>
2026-06-14 23:22:05 +0530

    Graph representation and dfs

--- src/Graphs/Graph.java
+++ src/Graphs/Graph.java
@@ -1,5 +1,7 @@
 package Graphs;
 
+import java.util.Arrays;
+
 public class Graph {
     int V;                       //no. of vertices
     int [][] graph;              //2D matrix
@@ -7,6 +9,22 @@ public class Graph {
         this.V = V;
         this.graph = new int[V][V];
     }
+    public void dfs(int start){
+        boolean[] visited = new boolean[V];
+        Arrays.fill(visited,false);
+        dfsUtil(start, visited);
+
+    }
+    public void dfsUtil(int start, boolean[] visited){
+        visited[start] = true;
+        System.out.print(start+" ");
+        for (int v = 0; v < V; v++) {
+            if(graph[start][v]==1 && !visited[v]){
+                dfsUtil(v,visited);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Graph g = new Graph(5);
         g.graph = new int[][]{
@@ -16,5 +34,7 @@ public class Graph {
                 {0,1,0,0,1},
                 {0,0,1,1,0},
         };
+        g.dfs(0);
+        System.out.println();
     }
 }
---
+++ src/graphRepresentation/adjancetList.java
@@ -0,0 +1,40 @@
+package graphRepresentation;
+
+import java.util.ArrayList;
+
+public class adjancetList {
+    static class edge{
+        int src;
+        int dest;
+        public edge(int s, int d){
+            this.src =s;
+            this.dest = d ;
+        }
+    }
+    public static void createGraph(ArrayList<edge> graph[]){
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<edge>();
+        }
+        graph[0].add(new edge(0,2));
+
+        graph[1].add(new edge(1,2));
+        graph[1].add(new edge(1,3));
+
+        graph[2].add(new edge(2,0));
+        graph[2].add(new edge(2,1));
+        graph[2].add(new edge(2,3));
+
+        graph[3].add(new edge(3,1));
+        graph[3].add(new edge(3,2));
+    }
+    public static void main(String[] args) {
+        int v=4;
+        ArrayList<edge> graph[] = new ArrayList[v];
+        createGraph(graph);
+
+        for (int i = 0; i < graph[1].size(); i++) {
+            edge e = graph[1].get(i);
+            System.out.print(e.dest + " ");
+        }
+    }
+}
